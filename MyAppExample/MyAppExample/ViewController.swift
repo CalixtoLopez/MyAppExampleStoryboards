4484d68fe82b5ece5bc667b8f2b77d73db835eb9
Calixto <calixto.lopez.santos@gmail.com>
2025-03-27 11:24:46 +0100

    First commit

---
+++ MyAppExample/MyAppExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAppExample
+//
+//  Created by Calixto Lopez Santos on 27/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
